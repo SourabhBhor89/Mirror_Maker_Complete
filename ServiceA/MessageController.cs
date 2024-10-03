bd382ff0e332facd425f14ab9bb3950562292eb0
Sourabh Bhor <sourabhbhor89@gmail.com>
2024-10-03 12:45:09 +0530

    Final

--- ServiceA/MessageController.cs
+++ ServiceA/MessageController.cs
@@ -41,32 +41,5 @@ public async Task<IActionResult> SendMessage([FromBody] MessageDto messageDto)
         return StatusCode(500, "Error sending message");
     }
 }
-
-
-
-
-
-
-
-
-    // [HttpPost("send")]
-    // public async Task<IActionResult> SendMessage([FromBody] MessageDto messageDto)
-    // {
-    //     if (messageDto == null || string.IsNullOrEmpty(messageDto.Message))
-    //     {
-    //         return BadRequest("Message cannot be empty");
-    //     }
-
-    //     try
-    //     {
-    //         await _kafkaProducerService.ProduceAsync("service-a-topic", messageDto.Message);
-    //         return Ok("Message sent to service-a-topic");
-    //     }
-    //     catch
-    //     {
-    //         return StatusCode(500, "Error sending message");
-    //     }
-    // }
-
     
 }
